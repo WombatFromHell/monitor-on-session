51781c0e8b2187493225ad7181066dfb2624f6b3
WombatFromHell <WombatFromHell@users.noreply.github.com>
2024-04-29 13:28:33 -0600

    Add a readme and some comments

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is a quick hack to workaround KDE Plasma 6.0's lack of a Lock/Unlock event.
+
+Run the `./setup.sh` with the `install` or `uninstall` keywords to enable or disable the on-session-state.service. Edit the `on-session.sh` script to change what gets run and when.
--- on-session.sh
+++ on-session.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "true" ]; then
-	# do nothing here
-	sleep 1
+	#
+	# this runs when the session goes into a locked state
+	#
+	echo ""
 elif [ "$1" == "false" ]; then
+	#
+	# these things run when the session goes into an unlocked state
+	#
 	# do a refresh rate toggle to fix gsync on Nvidia cards
 	# ... at least until the next Monitor Suspend/Power event
 	$HOME/.local/bin/fix-gsync.sh
