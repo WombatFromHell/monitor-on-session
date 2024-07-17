5b4fbd85c62356aaab3f0c681cbb74d516d7dc9c
WombatFromHell <WombatFromHell@users.noreply.github.com>
2024-07-17 13:40:29 -0600

    Added logic to avoid refresh sync on gnome

--- fix-gsync.sh
+++ fix-gsync.sh
@@ -3,19 +3,32 @@
 OUTPUT="DP-4"
 XRANDR=$(command -v xrandr)
 
+get_current_primary_monitor() {
+  SELECTED=$(xrandr | grep " connected primary " | awk '{print $1}')
+  echo "$SELECTED"
+}
+CURRENT_PRIMARY=$(get_current_primary_monitor)
+
 if ! command -v nvidia-settings &>/dev/null; then
-	echo "ERROR: nvidia-settings not found in PATH, aborting!"
-	exit 1
+  echo "ERROR: nvidia-settings not found in PATH, aborting!"
+  exit 1
 fi
 VRR_ENABLED=$(nvidia-settings -q AllowVRR | awk -F':' '/Attribute/ {print $3}' | sed 's/^ //;s/\.//g')
 
-if [ "$XDG_SESSION_TYPE" = "x11" ] && [ "$VRR_ENABLED" -eq 1 ]; then
-	"$XRANDR" --output "$OUTPUT" -r 120 --mode 2560x1440
-	sleep 1
-	"$XRANDR" --output "$OUTPUT" -r 144 --mode 2560x1440
-	"$@"
+if [ "$XDG_SESSION_TYPE" = "x11" ] && ! [[ "$DESKTOP_SESSION" = *gnome* ]] && [ "$VRR_ENABLED" -eq 1 ] && [ "$CURRENT_PRIMARY" = "$OUTPUT" ]; then
+  "$XRANDR" --output "$OUTPUT" -r 120 --mode 2560x1440
+  sleep 1
+  "$XRANDR" --output "$OUTPUT" -r 144 --mode 2560x1440
+  "$@"
 elif ! [ "$XDG_SESSION_TYPE" = "x11" ]; then
-	echo "Warning: not running in X11 session! XRANDR will not work!"
+  echo "Warning: not running in X11 session! XRANDR will not work!"
+  exit 1
 elif [ "$VRR_ENABLED" -eq 0 ]; then
-	echo "Warning: VRR is not enabled, aborting!"
+  echo "Warning: VRR is not enabled, aborting!"
+  exit 1
+elif ! [ "$CURRENT_PRIMARY" = "$OUTPUT" ]; then
+  echo "Warning: the current primary monitor is not $OUTPUT!"
+  exit 1
 fi
+
+exit 0
--- monitor-dbus-session-state.sh
+++ monitor-dbus-session-state.sh
@@ -1,48 +1,72 @@
 #!/usr/bin/env bash
+# Path to the script to run when the signal is received
+SCRIPT_PATH="${1:-$HOME/.local/bin/on-session.sh}"
+LOG_FILE="/tmp/unlock_monitor.log"
 
-lock_scripts=()
-unlock_scripts=("$HOME/.local/bin/fix-gsync.sh")
-
-run_scripts() {
-	for script in "${@}"; do
-		"$script"
-		exit_code=$?
-		if [ "$exit_code" -ne 0 ]; then
-			echo "Error: $script exited with code $exit_code"
-		fi
-	done
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >>"$LOG_FILE"
 }
 
 handle_signal() {
-	echo "Screen saver active state changed to: $1"
-
-	if [ "$1" == "true" ]; then
-		#
-		# this runs when the session goes into a locked state
-		#
-		echo "Running lock state scripts..."
-		run_scripts "${lock_scripts[@]}"
-	elif [ "$1" == "false" ]; then
-		#
-		# these things run when the session goes into an unlocked state
-		#
-		echo "Running unlock state scripts..."
-		run_scripts "${unlock_scripts[@]}"
-	fi
+  log "Screen lock state changed to: $1"
+  log "Running: $SCRIPT_PATH $1"
+  if [[ -x "$SCRIPT_PATH" ]]; then
+    "$SCRIPT_PATH" "$1"
+  else
+    log "Error: $SCRIPT_PATH is not executable or does not exist."
+  fi
 }
 
-prev_state=""
-dbus-monitor --session "type='signal',interface='org.freedesktop.ScreenSaver'" | (
-	while true; do
-		read -r line
-		if [[ "$line" == *"member=ActiveChanged"* ]]; then
-			object_path=$(echo "$line" | awk -F'path=' '{print $2}' | awk -F';' '{print $1}')
-			active=$(dbus-send --session --print-reply --dest=org.freedesktop.ScreenSaver "$object_path" org.freedesktop.ScreenSaver.GetActive)
-			state=$(echo "$active" | awk '/boolean/ {print $2}' | tr -d '"')
-			if [ "$state" != "$prev_state" ]; then
-				handle_signal "$state"
-				prev_state="$state"
-			fi
-		fi
-	done
-)
+detect_desktop_environment() {
+  if [[ -n "$XDG_CURRENT_DESKTOP" ]]; then
+    echo "$XDG_CURRENT_DESKTOP"
+  elif [[ -n "$DESKTOP_SESSION" ]]; then
+    echo "$DESKTOP_SESSION"
+  elif pgrep gnome-shell >/dev/null; then
+    echo "GNOME"
+  elif pgrep plasmashell >/dev/null; then
+    echo "KDE"
+  else
+    echo "UNKNOWN"
+  fi
+}
+
+if [[ ! -x "$SCRIPT_PATH" ]]; then
+  echo "Error: $SCRIPT_PATH is not executable or does not exist."
+  exit 1
+fi
+
+DE=$(detect_desktop_environment)
+log "Detected desktop environment: $DE"
+
+case "$DE" in
+*GNOME*)
+  DBUS_INTERFACE="org.gnome.ScreenSaver"
+  OBJECT_PATH="/org/gnome/ScreenSaver"
+  ;;
+*KDE*)
+  DBUS_INTERFACE="org.freedesktop.ScreenSaver"
+  OBJECT_PATH="/org/freedesktop/ScreenSaver"
+  ;;
+*)
+  log "Unsupported desktop environment: $DE"
+  exit 1
+  ;;
+esac
+
+log "Starting screen lock monitor for $DE"
+
+dbus-monitor --session "type='signal',interface='$DBUS_INTERFACE'" |
+  while read -r line; do
+    if [[ "$line" == *"member=ActiveChanged"* ]]; then
+      active=$(dbus-send --session --print-reply --dest="$DBUS_INTERFACE" \
+        "$OBJECT_PATH" "$DBUS_INTERFACE.GetActive")
+      state=$(echo "$active" | awk '/boolean/ {print $2}')
+      if [[ "$state" != "$prev_state" ]]; then
+        handle_signal "$state"
+        prev_state="$state"
+      fi
+    fi
+  done
+
+log "Screen lock monitor stopped"
